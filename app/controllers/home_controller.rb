d33a0927f1ad2e7c6b6f1e1baeb7a21d7abc9172
Djalal Hajiyev <djalal.hajiyev@telefonica.com>
2021-03-29 14:26:41 +0200

    10 per site

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @recipe = Recipe.paginate(page: params[:page], per_page: 2)
+    @recipe = Recipe.paginate(page: params[:page], per_page: 10)
   end
 end
