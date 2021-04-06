dda800f78b0a1cd5885b7f71b29740b29c5e789a
Djalal Hajiyev <djalal.hajiyev@telefonica.com>
2021-04-06 15:36:32 +0200

    search form in recipe index

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,6 +1,7 @@
 class RecipeController < ApplicationController
   def index
-    @recipe = Recipe.paginate(page: params[:page], per_page: 10).order("updated_at DESC")
+    @q = Recipe.ransack(params[:q])
+    @recipe = @q.result(distinct: true).paginate(page: params[:page], per_page: 15).order("updated_at DESC")
   end
 
   def new
