064cc251d842a310558dc292e441b9eb7419e6c4
Djalal Hajiyev <djalal.hajiyev@telefonica.com>
2021-04-07 13:36:00 +0200

    grid view feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :gridview]
+
   def index
     @q = Recipe.ransack(params[:q])
     @recipe = @q.result(distinct: true).paginate(page: params[:page], per_page: 10).order("updated_at DESC")
   end
+
+  def gridview
+    @q = Recipe.ransack(params[:q])
+    @recipe = @q.result(distinct: true).paginate(page: params[:page], per_page: 10).order("updated_at DESC")
+  end
 end
+
