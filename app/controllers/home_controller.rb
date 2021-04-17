e884925caced386386adb23d55903c1ede623f64
Djalal Hajiyev <djalal.hajiyev@telefonica.com>
2021-04-17 11:12:07 +0200

    recipes will show randomly

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,12 @@ class HomeController < ApplicationController
 
   def index
     @q = Recipe.ransack(params[:q])
-    @recipe = @q.result(distinct: true).paginate(page: params[:page], per_page: 10).order("updated_at DESC")
+    @recipe = @q.result(distinct: true).paginate(page: params[:page], per_page: 10).order("random()")
   end
 
   def gridview
     @q = Recipe.ransack(params[:q])
-    @recipe = @q.result(distinct: true).paginate(page: params[:page], per_page: 10).order("updated_at DESC")
+    @recipe = @q.result(distinct: true).paginate(page: params[:page], per_page: 10).order("random()")
   end
 end
 
