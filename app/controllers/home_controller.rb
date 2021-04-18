bf5f9fe9251958125c74e2ed6aca4d5379af101a
Djalal Hajiyev <djalal.hajiyev@telefonica.com>
2021-04-18 20:31:54 +0200

    bigger images in table view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,12 @@ class HomeController < ApplicationController
 
   def index
     @q = Recipe.ransack(params[:q])
-    @recipe = @q.result(distinct: true).paginate(page: params[:page], per_page: 10).order("created_at DESC")
+    @recipe = @q.result(distinct: true).paginate(page: params[:page], per_page: 10).order("updated_at DESC")
   end
 
   def gridview
     @q = Recipe.ransack(params[:q])
-    @recipe = @q.result(distinct: true).paginate(page: params[:page], per_page: 10).order("created_at DESC")
+    @recipe = @q.result(distinct: true).paginate(page: params[:page], per_page: 10).order("updated_at DESC")
   end
 end
 
