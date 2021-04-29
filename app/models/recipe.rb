a614f068bfc0ce52997ed335acc280528e76a57b
Djalal <hajiyev.djalal@gmail.com>
2021-04-29 08:25:32 +0200

    searching in description as well

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ApplicationRecord
   has_many :days
   has_one_attached :main_image
   has_rich_text :description
+  has_one :action_text_rich_text, class_name: "ActionText::RichText", as: :record
 end
