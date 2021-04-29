599d33a960be776a3ae24e4b3983d04f3dbc8dd5
Djalal Hajiyev <djalal.hajiyev@telefonica.com>
2021-04-29 12:47:21 +0200

    deployment fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
+  # Code is not reloaded between requests
   config.cache_classes = true
   config.hosts << "tasty-crumb.herokuapp.com"
 
