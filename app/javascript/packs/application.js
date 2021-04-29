14d3e78dd41a3517156267a8ce7646ab42d2e515
Djalal Hajiyev <djalal.hajiyev@telefonica.com>
2021-04-29 09:08:54 +0200

    deployment fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("@rails/activestorage").start();
 require("channels");
 require("jQuery");
 
+import "@fortawesome/fontawesome-free/css/all.css";
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -19,5 +21,5 @@ require("jQuery");
 window.jQuery = $;
 window.$ = $;
 
-require("trix")
-require("@rails/actiontext")
+require("trix");
+require("@rails/actiontext");
