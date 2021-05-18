73f24d4b5f7b64c2c5ded91dfb482f558a9bd51b
Djalal Hajiyev <djalal.hajiyev@telefonica.com>
2021-05-18 08:31:38 +0200

    integration tests in the CI

--- spec/features/recipe_scene.rb
+++ spec/features/recipe_scene.rb
@@ -2,6 +2,10 @@ require "rails_helper"
 
 RSpec.describe "root path", type: :system do
   describe "searchbar" do
+    before do
+      @recipe = Recipe.create(title: "Reisnudelsalat mit gebackenem Tofu")
+    end
+
     it "has a search bar" do
       visit root_path
       expect(page).to have_css("#recipe_search")
