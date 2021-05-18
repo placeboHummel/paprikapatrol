8eb6e45227c85e89d97a1112ba9e21869411d515
Djalal Hajiyev <djalal.hajiyev@telefonica.com>
2021-05-18 10:12:56 +0200

    trying to fix selenium webdriver error on github actions

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,9 +4,12 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
 require "rspec/rails"
 require "capybara/rails"
 require "capybara/rspec"
+
+Capybara.javascript_driver = :selenium_chrome_headless
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
