5c276a964a74161c4555a0fae61b421c9ca739aa
Djalal Hajiyev <djalal.hajiyev@telefonica.com>
2021-05-18 12:11:34 +0200

    trying to fix selenium webdriver error on github actions

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -11,7 +11,7 @@ Capybara.register_driver :chrome_headless do |app|
   options.add_argument("--headless")
   options.add_argument("--no-sandbox")
   options.add_argument("--disable-dev-shm-usage")
-  options.add_argument("--window-size=1400,1400")
+  options.add_argument("--window-size=1920x1080")
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
