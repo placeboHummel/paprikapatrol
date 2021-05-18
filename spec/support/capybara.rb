4efe0fd111a30be610b2adcc12698743a922ce6f
Djalal Hajiyev <djalal.hajiyev@telefonica.com>
2021-05-18 11:44:57 +0200

    trying to fix selenium webdriver error on github actions

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,28 +1,21 @@
-RSpec.configure do |config|
-  config.include Capybara::DSL
+require "capybara/rails"
+require "capybara/rspec"
+require "selenium/webdriver"
 
-  Capybara.server = :puma, { Silent: true }
-
-  # Chrome non-headless driver
-  Capybara.register_driver :chrome do |app|
-    Capybara::Selenium::Driver.new(app, browser: :chrome)
-  end
-
-  # Chrome headless driver
-  Capybara.register_driver :headless_chrome do |app|
-    caps = Selenium::WebDriver::Remote::Capabilities.chrome(loggingPrefs: { browser: "ALL" })
-    opts = Selenium::WebDriver::Chrome::Options.new
-
-    chrome_args = %w[--headless --no-sandbox --disable-gpu --window-size=1920,1080 --remote-debugging-port=9222]
-    chrome_args.each { |arg| opts.add_argument(arg) }
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: opts, desired_capabilities: caps)
-  end
-
-  # Switch between :chrome / :headless_chrome to see tests run in chrome
-  case ENV["HEADLESS"]
-  when "true", 1, nil
-    Capybara.javascript_driver = :headless_chrome
-  else
-    Capybara.javascript_driver = :headless_chrome
-  end
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome, args: ["--window-size=1280,1024"])
 end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu no-sandbox --window-size=1280,1024) },
+  )
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    desired_capabilities: capabilities,
+  )
+end
+
+Capybara.javascript_driver = :headless_chrome
