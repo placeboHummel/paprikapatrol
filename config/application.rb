823c3dd0508c0732177374cce1fda7509962af50
Djalal Hajiyev <djalal.hajiyev@telefonica.com>
2021-06-01 16:51:56 +0200

    changed app name

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,11 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RecipeApp
+module PaprikaPatrol
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.hosts << "recipe.halfpace.de"
+    config.hosts << "paprikapatrol.de"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
