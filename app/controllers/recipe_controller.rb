b1962e41862445c6e14e4facae8cb6e436b21bc5
Djalal Hajiyev <djalal.hajiyev@telefonica.com>
2021-06-07 15:53:15 +0200

    small layout changes

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,5 +1,6 @@
 class RecipeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :verify_authenticity_token
 
   def index
     @q = Recipe.ransack(params[:q])
